49f09d884ee54be9df71f3f2d40a633ec5b27363
NagaiKoki <kokinagai10+1@gmail.com>
2020-11-08 11:01:08 +0900

    headerコンポーネントを作成

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+// import components
+import Header from '../src/components/Header'
+
+const Index: React.FC = () => {
+  return (
+    <Header />
+  )
+}
+
+export default Index
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>hoge</h1>
+      
+      <style jsx>{`
+        header {
+          background: red;
+        }
+      `}
+      </style>
+    </header>
+  )
+}
+
+export default Header
